e7e46b4706993a0ef900031842b7d905a1c1ffd6
Stella Mazzillo <stellagress5@gmail.com>
2023-04-19 22:46:46 -0400

    complete sending data with fetch lab

--- index.js
+++ index.js
@@ -1,1 +1,83 @@
 // Add your code here
+
+function submitData(userName, userEmail) {
+
+
+
+return fetch('http://localhost:3000/users', {
+method : 'POST',
+headers : {
+    'Content-Type' : 'application/json',
+    'Accept' : 'application/json',
+},
+
+body : JSON.stringify({
+name: userName,
+email: userEmail
+})
+})
+
+.then(res => res.json())
+.then(function (data) {
+document.body.innerHTML = data.id
+})
+
+.catch(function(error) {
+    //alert("You got an error")
+    //console.log(error.message)
+
+    document.body.innerHTML = error.message
+});
+
+}
+
+
+
+
+
+
+
+
+
+// let usersTag = document.createElement('div')
+// document.querySelector('body').appendChild(usersTag)
+// I thought I would have to use this, however, not necesary 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function submitData(userName, userEmail) {
+
+
+
+//     return fetch('http://localhost:3000/users', {
+//     method : 'POST',
+//     headers : {
+//         'Content-Type' : 'application/json',
+//         'Accept' : 'application/json',
+//     },
+    
+//     body : JSON.stringify({
+//     name: userName,
+//     email: userEmail
+    
+//     })
+//     })
+//     .then(res => res.json())
+//     .then(data => console.log(data))
+//     }
+    
+
+
+
+
+
